0f84b230ef0adad04e144d8fedf5c0bbf4e43185
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-10-02 17:34:24 -0500

    fixes: merge conflicts

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col, Container } from "reactstrap";
 import { useHistory } from "react-router-dom";
 import RecipeDisplaySelect from "./RecipeDisplaySelect";
